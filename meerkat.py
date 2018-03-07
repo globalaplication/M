5773e9fe7247d746b071c1a1fb7bf894888fd333
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-03-07 14:18:44 +0300

    Update meerkat.py

--- meerkat.py
+++ meerkat.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import gi, urllib, os
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import Gio, Gdk
 from gi.repository import GdkPixbuf, GLib
-
 class Gdeb(Gtk.FileChooserDialog):
     SelectDebFiles = []
     ProjeName = ''
@@ -25,23 +25,28 @@ class Gdeb(Gtk.FileChooserDialog):
         if os.path.isdir(DebMakeFolder) is False:
             os.makedirs(DebMakeFolder)
         os.system('touch '+DebMakeFolder+'/control')
-        for makefile in zip([self.PackageEntry.get_text(), self.VersionEntry.get_text(), self.SectionEntry.get_text(), 
+        for makefile in zip([self.PackageEntry.get_text(), self.VersionEntry.get_text(), self.SectionEntry.get_text(),
                 self.PriorityEntry.get_text(), self.ContactEntry.get_text(), self.CpuEntry.get_active_text(),
-                    self.DescriptionEntry.get_text()], 
-                        ['Package:', 'Version:', 'Section:', 'Priority:', 
+                    self.DescriptionEntry.get_text()],
+                        ['Package:', 'Version:', 'Section:', 'Priority:',
                             'Maintainer:', 'Architecture:', 'Description:']):
             DebMakeString = DebMakeString + makefile[1]+chr(32)+makefile[0] +'\n'
         with open(DebMakeFolder+'/control', 'w') as control:
             control.write(DebMakeString)
-        for makefile in zip(['Version', 'Name', 'Encoding', 'Comment', 
+        for makefile in zip(['Version', 'Name', 'Encoding', 'Comment',
                 'Path', 'Exec', 'Icon', 'Terminal', 'Type', 'Categories'],
-                    [self.VersionEntry.get_text(), self.PackageEntry.get_text(), 'UTF-8', self.DescriptionEntry.get_text(), 
-                        '/usr/local/bin/'+self.ProjeName, 
-                            self.ExceComboBoxText.get_active_text().lower()+' '+self.PyDict[self.MainComboBoxText.get_text()]['py'], 
+                    [self.VersionEntry.get_text(), self.PackageEntry.get_text(), 'UTF-8', self.DescriptionEntry.get_text(),
+                        '/usr/local/bin/'+self.ProjeName,
+                            self.ExceComboBoxText.get_active_text().lower()+' '+self.PyDict[self.MainComboBoxText.get_text()]['py'],
                             self.DesktopIconDict[self.DesktopIconComboBoxText.get_text()]['png'],
-                            'false', 'Application', 
+                            'false', 'Application',
                             self.CategoriesComboBoxText.get_active_text()[0:self.CategoriesComboBoxText.get_active_text().find(' ')]+';']):
             DesktopMakeString = DesktopMakeString + str(makefile[0]+'='+makefile[1]) + '\n'
+        TurkishChrControl = self.PackageEntry.get_text() + self.DesktopIconDict[self.DesktopIconComboBoxText.get_text()]['png'] + self.MainComboBoxText.get_text()
+        for ch in ["ö","ı","ç","ş","ü", "Ö", "I", "Ş", "Ç", "Ü"]:
+            if TurkishChrControl.find(ch) is not -1:
+                self.message()
+                break
         with open(self.DesktopFilePath+'/'+self.PackageEntry.get_text()+'.desktop', 'w') as control:
             control.write('[Desktop Entry]\n'+DesktopMakeString)
         os.system('dpkg-deb --build '+ self.ProjePath+'/'+self.ProjeName)
@@ -80,9 +85,9 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.PackageLabel = Gtk.Label("Proje", xalign=0)
         self.PackageEntry = Gtk.Entry()
-        self.PackageEntry.set_width_chars(24)
+        self.PackageEntry.set_width_chars(23)
         self.PackageEntry.set_sensitive(False)
-        self.PackageEntry.set_text('') 
+        self.PackageEntry.set_text('')
         self.Hbox.pack_start(self.PackageLabel, True, True, 0)
         self.Hbox.pack_start(self.PackageEntry, False, True, 0)
         self.ListBox.add(self.BoxRow)
@@ -91,9 +96,9 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.ExceLabel = Gtk.Label("Exce", xalign=0)
         self.ExceComboBoxText = Gtk.ComboBoxText()
-        self.ExceComboBoxText.insert(0, "0", "Python"+' '*35)
-        self.ExceComboBoxText.insert(1, "1", "Python3") 
-        self.ExceComboBoxText.set_active(0)    
+        self.ExceComboBoxText.insert(0, "0", "Python"+' '*30)
+        self.ExceComboBoxText.insert(1, "1", "Python3")
+        self.ExceComboBoxText.set_active(0)
         self.ExceComboBoxText.set_sensitive(True)
         self.Hbox.pack_start(self.ExceLabel, True, True, 0)
         self.Hbox.pack_start(self.ExceComboBoxText, False, True, 0)
@@ -106,7 +111,7 @@ class Gdeb(Gtk.FileChooserDialog):
         self.MainButton = Gtk.Button()
         self.MainButton.connect("clicked", self.SelectAppFileFileChooser, self.MainComboBoxText)
         #self.MainComboBoxText.set_wrap_width(25)
-        #self.MainComboBoxText.insert(0, "0", "Python")  
+        #self.MainComboBoxText.insert(0, "0", "Python")
         self.MainComboBoxText.set_sensitive(True)
         self.Hbox.pack_start(self.MainLabel, True, True, 0)
         self.Hbox.pack_start(self.MainComboBoxText, False, True, 0)
@@ -120,19 +125,29 @@ class Gdeb(Gtk.FileChooserDialog):
         self.DesktopIconButton = Gtk.Button()
         self.DesktopIconButton.connect("clicked", self.SelectAppFileFileChooser, self.DesktopIconComboBoxText)
         #self.DesktopIconComboBoxText.insert(0, "0", "Python")
-        #self.DesktopIconComboBoxText.insert(1, "1", "Python3"+' '*26) 
+        #self.DesktopIconComboBoxText.insert(1, "1", "Python3"+' '*26)
         self.DesktopIconComboBoxText.set_sensitive(True)
         self.Hbox.pack_start(self.DesktopIconLabel, True, True, 0)
         self.Hbox.pack_start(self.DesktopIconComboBoxText, False, True, 0)
         self.Hbox.pack_start(self.DesktopIconButton, False, True, 0)
-        self.ListBox.add(self.BoxRow)
 
+        self.ListBox.add(self.BoxRow)
+        self.BoxRow = Gtk.ListBoxRow()
+        self.Hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=1)
+        self.BoxRow.add(self.Hbox)
+        self.DesktopIconLabelWarning = Gtk.Label("", xalign=0)
+        self.DesktopIconComboBoxTextWarning = Gtk.Label("Simge Maksimum 64x64" + " "*10 + "\nolmalı")
+        self.DesktopIconComboBoxTextWarning.set_sensitive(True)
+        self.Hbox.pack_start(self.DesktopIconLabelWarning, True, True, 0)
+        self.Hbox.pack_start(self.DesktopIconComboBoxTextWarning, False, True, 0)
+
+        self.ListBox.add(self.BoxRow)
         self.BoxRow = Gtk.ListBoxRow()
         self.Hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         self.BoxRow.add(self.Hbox)
         self.VersionLabel = Gtk.Label("Versiyon", xalign=0)
         self.VersionEntry = Gtk.Entry()
-        self.VersionEntry.set_width_chars(24)
+        self.VersionEntry.set_width_chars(23)
         self.VersionEntry.set_text('0.1')
         self.Hbox.pack_start(self.VersionLabel, True, True, 0)
         self.Hbox.pack_start(self.VersionEntry, False, True, 0)
@@ -149,11 +164,11 @@ class Gdeb(Gtk.FileChooserDialog):
         self.CategoriesComboBoxText.insert(4, "4", "Settings ")
         self.CategoriesComboBoxText.insert(5, "5", "System ")
         self.CategoriesComboBoxText.insert(6, "6", "Utility ")
-        self.CategoriesComboBoxText.insert(7, "7", "Development"+' '*23)
+        self.CategoriesComboBoxText.insert(7, "7", "Development"+' '*19)
         self.CategoriesComboBoxText.insert(8, "8", "Education ")
         self.CategoriesComboBoxText.insert(9, "9", "Game ")
         self.CategoriesComboBoxText.insert(10, "10", "Science ")
-        self.CategoriesComboBoxText.set_active(7)    
+        self.CategoriesComboBoxText.set_active(7)
         self.CategoriesComboBoxText.set_sensitive(True)
         self.Hbox.pack_start(self.CategoriesLabel, True, True, 0)
         self.Hbox.pack_start(self.CategoriesComboBoxText, False, True, 0)
@@ -163,7 +178,7 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.SectionLabel = Gtk.Label("Section", xalign=0)
         self.SectionEntry = Gtk.Entry()
-        self.SectionEntry.set_width_chars(24)
+        self.SectionEntry.set_width_chars(23)
         self.SectionEntry.set_text('base')
         self.Hbox.pack_start(self.SectionLabel, True, True, 0)
         self.Hbox.pack_start(self.SectionEntry, False, True, 0)
@@ -173,7 +188,7 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.PriorityLabel = Gtk.Label("Priority", xalign=0)
         self.PriorityEntry = Gtk.Entry()
-        self.PriorityEntry.set_width_chars(24)
+        self.PriorityEntry.set_width_chars(23)
         self.PriorityEntry.set_text('optional')
         self.Hbox.pack_start(self.PriorityLabel, True, True, 0)
         self.Hbox.pack_start(self.PriorityEntry, False, True, 0)
@@ -183,7 +198,7 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.ContactLabel = Gtk.Label("İletişim", xalign=0)
         self.ContactEntry = Gtk.Entry()
-        self.ContactEntry.set_width_chars(24)
+        self.ContactEntry.set_width_chars(23)
         self.ContactEntry.set_text('@glabald')
         self.Hbox.pack_start(self.ContactLabel, True, True, 0)
         self.Hbox.pack_start(self.ContactEntry, False, True, 0)
@@ -193,7 +208,7 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow.add(self.Hbox)
         self.DescriptionLabel = Gtk.Label("Açıklama", xalign=0)
         self.DescriptionEntry = Gtk.Entry()
-        self.DescriptionEntry.set_width_chars(24)
+        self.DescriptionEntry.set_width_chars(23)
         self.DescriptionEntry.set_text('Masaüstü Uygulaması')
         self.Hbox.pack_start(self.DescriptionLabel, True, True, 0)
         self.Hbox.pack_start(self.DescriptionEntry, False, True, 0)
@@ -201,12 +216,12 @@ class Gdeb(Gtk.FileChooserDialog):
         self.BoxRow = Gtk.ListBoxRow()
         self.Hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         self.BoxRow.add(self.Hbox)
-        self.CpuLabel = Gtk.Label("İslemci Mimarisi", xalign=0)
+        self.CpuLabel = Gtk.Label("İslemci", xalign=0)
         self.CpuEntry = Gtk.ComboBoxText()
         self.CpuEntry.insert(0, "0", "i386")
-        self.CpuEntry.insert(1, "1", "i684")
+        self.CpuEntry.insert(1, "1", "amd64")
         self.CpuEntry.set_sensitive(True)
-        self.CpuEntry.set_active(0)   
+        self.CpuEntry.set_active(0)
         #self.CpuEntry.set_text('i386') #'platform.machine()' 'amd64'
         self.Hbox.pack_start(self.CpuLabel, True, True, 0)
         self.Hbox.pack_start(self.CpuEntry, False, True, 0)
@@ -310,7 +325,7 @@ class Gdeb(Gtk.FileChooserDialog):
         filter_py.set_name("Python files")
         filter_py.add_mime_type("text/x-python")
         filter_py.add_pattern("*.pyc")
-        dialog.add_filter(filter_py)     
+        dialog.add_filter(filter_py)
     def Run(self, path):
         self.SelectDebFiles = []
         self.ProjeName = os.path.split(path)[1]
@@ -324,13 +339,13 @@ class Gdeb(Gtk.FileChooserDialog):
                     self.SelectDebFiles.append(j[0]+'/'+sub)
         for source in self.SelectDebFiles:
             if (source[-4:] == '.deb'): continue
-            if (source[-4:] == '.png' or source[-4:] == '.ico'): 
+            if (source[-4:] == '.png' or source[-4:] == '.ico'):
                 #self.DesktopIconComboBoxText.append_text(os.path.split(source)[1])
                 PngFile = os.path.split(source)[1]
                 PngPath = '/usr/local/bin/'+os.path.split(source)[0][os.path.split(source)[0].find(self.ProjeName):]+'/'+PngFile
-                #self.DesktopIconComboBoxText.set_active(0) 
+                #self.DesktopIconComboBoxText.set_active(0)
                 self.DesktopIconDict[PngFile] = {'png':PngPath}
-            if (source[-3:] == '.py' or source[-4:] == '.pyc'): 
+            if (source[-3:] == '.py' or source[-4:] == '.pyc'):
                 #self.MainComboBoxText.append_text(os.path.split(source)[1])
                 PyFile = os.path.split(source)[1]
                 PyPath = '/usr/local/bin/'+os.path.split(source)[0][os.path.split(source)[0].find(self.ProjeName):]+'/'+PyFile
@@ -359,6 +374,13 @@ class Gdeb(Gtk.FileChooserDialog):
             Gtk.ButtonsType.OK, "")
         dialog.format_secondary_text(
             "Deb Paket Dosyası Oluşturuldu.")
+        dialog.run()
+        dialog.destroy()
+    def message(self):
+        dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "")
+        dialog.format_secondary_text(
+            "Proje dosya ve klasörlerde türkçe karekter ve boşluk kullanılmamalı!")
         dialog.run()
         dialog.destroy()
 def main(BetaApp=None):
